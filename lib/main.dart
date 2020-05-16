b7f5415844e68b63db5695db08b33cef5fd69592
“andi <andidarusman92@gmail.com>
2020-05-16 22:16:17 +0700

    menambahkan floating button

--- lib/main.dart
+++ lib/main.dart
@@ -14,22 +14,33 @@ class Widget1 extends StatelessWidget{
   Widget build(BuildContext context) {
     //buat scaffold
     return new Scaffold(
-      //buat halaman bagian title
-      //dan halaman utama warna background hijau
-      backgroundColor: Colors.green,
+      //buat bagian title
+      //dan halaman utama warna background berwana
+      backgroundColor: Colors.yellow[200],
       appBar: new AppBar(
         title: new Text("Flutter App #1"),
-        //memberikan tulisan dengan backgroud merah
-        backgroundColor: Colors.redAccent,
+        //memberikan tulisan dengan backgroud berwarna
+        backgroundColor: Color(0xFFFF7043),
       ),
+
       //buat halaman bagian body
       body: new Center(
-        //buat text warna kuning
+        //buat text warna
         child: new Text(
             "Ini Stateless Widget"
-        ,style: TextStyle(color: Colors.yellowAccent),
+        ,style: TextStyle(color: Colors.red[400]),
         ),
       ),
+
+      //menambahkan tombol floating action button
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Camera',
+        //menggunakan icon camera
+        child: Icon(Icons.camera),
+        backgroundColor: Colors.blue[400],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
